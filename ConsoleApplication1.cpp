a178a9aacb0909d8fdf0062b5bd57fb1ffefc0e8
BLVCKDIAMONDSKY <157357160+BLVCKDIAMONDSKY@users.noreply.github.com>
2025-02-25 09:15:16 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+
+    int x;
+
+    cout << "Enter integer x\n";
+    cin >> x;
+
+    float y = log(x);
+
+    cout << "y: "<< y;
+
+}
